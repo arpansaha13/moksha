e40d419e7456f59aadf393c347f654e3985d2a1c
arpansaha13 <arpansaha1300@gmail.com>
2023-02-13 16:35:23 +0530

    setup layout system

---
+++ src/pages/Timeline.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css';
+
+function Timeline() {
+  return (
+    <div>
+      timeline
+    </div>
+  );
+}
+
+export default Timeline;
--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,29 @@
-import React, {useState, useEffect} from 'react'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from "./pages/Home";
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import DefaultLayout from './layouts/default'
+import Home from "./pages/Home";
+import Timeline from "./pages/Timeline";
+import LoginPage from "./pages/auth/login";
+import SignUpPage from "./pages/auth/signup";
 
+/** Pass in the page component and get the page back with its layout. */
+function getPage(PageComponent) {
+  const getLayout = PageComponent.getLayout ?? (page => <DefaultLayout>{page}</DefaultLayout>)
+  return getLayout(<PageComponent />)
+}
 
 function routes() {
- 
-  
-
-
   return (
-    <div className='abc'>
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-      <Route exact path="/" element={<Home />}></Route>
-       </Routes>
+        <Route exact path="/" element={getPage(Home)} />
+        <Route exact path="/timeline" element={getPage(Timeline)} />
+        <Route exact path="/auth/login" element={getPage(LoginPage)} />
+        <Route exact path="/auth/signup" element={getPage(SignUpPage)} />
+      </Routes>
     </BrowserRouter>
-    </div>
   );
 }
 
