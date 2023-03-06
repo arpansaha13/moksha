8bc008e8ff636af7a84bb82f3a031c8deb9df596
Arghajit08 <bhowmikarghajit@gmail.com>
2023-03-07 00:03:14 +0530

    Bugs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,9 +82,30 @@ TEMPLATES = [
     },
 ]
 
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL=True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'withCredentials'
+]
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# EXTERNAL_DATABASE_URL='postgres://mokshadb_user:gl5tTJZAZtxlS6tlu2X2kLowSZjZ6osc@dpg-cfli3u1a6gdjlmp5228g-a.singapore-postgres.render.com/mokshadb'
+# EXTERNAL_DATABASE_URL='postgres://mokshadb_user:gl5tTJZAZtxlS6tlu2X2kLowSZjZ6osc@dpg-cfli3u1a6gdjlmp5228g-a/mokshadb'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
