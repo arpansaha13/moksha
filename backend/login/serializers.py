01b2e67017d68c712aef490aa08acec64bc6e9d6
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2023-03-09 00:54:25 +0530

    Update serializers.py

--- backend/login/serializers.py
+++ backend/login/serializers.py
@@ -8,3 +8,8 @@ class UsersSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class SpecificSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['avatar_idx','name','institution_name','phone_no','email','username','user_id','logged_in']
