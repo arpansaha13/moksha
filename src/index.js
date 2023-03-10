5f36a60fd46e80e9fbd7bae936eb8aca13ed6923
arpansaha13 <arpansaha1300@gmail.com>
2023-03-10 20:53:17 +0530

    migrate to react 18

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from 'react-dom/client'
 import App from "./App"
 import DataProvider from './containers/DataProvider'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const domNode = document.getElementById('root')
+const root = createRoot(domNode)
+
+root.render(
+  <DataProvider>
+    <App />
+  </DataProvider>
 )
