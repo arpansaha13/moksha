08181ecf5dce74e6cb908c49585c5bcc8c716178
arpansaha13 <arpansaha1300@gmail.com>
2023-03-18 12:14:54 +0530

    fix database links

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=env('DB_DEV_URL'), conn_max_age=600),
+    'default': dj_database_url.config(default=env('DB_EXT_URL'), conn_max_age=600),
 }
 
 # Password validation
