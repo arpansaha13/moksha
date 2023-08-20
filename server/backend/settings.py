056847d27853b6e8bca0386d1a5dc7116b36452f
Keraskp <adityakiran.cs@gmail.com>
2023-08-20 23:49:41 +0530

    modified allowed hosts

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = is_dev
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', env('CLIENT_DOMAIN')]
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', env('CLIENT_DOMAIN')]
+ALLOWED_HOSTS = ['*']
 
 APPEND_SLASH = False
 
