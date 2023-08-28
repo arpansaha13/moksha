b25a4c8a1baf10198a2cbfedbd2d3b10125d8370
arpansaha13 <arpansaha1300@gmail.com>
2023-08-28 13:59:11 +0530

    fix picture import path

--- client/src/components/Contests/RegisteredSoloContestCard.jsx
+++ client/src/components/Contests/RegisteredSoloContestCard.jsx
@@ -3,7 +3,7 @@ import { capitalCase } from 'change-case'
 import Sheet from '~common/Sheet'
 import MLink from '~common/Links/MLink'
 import DLink from '~common/Links/DLink'
-import ContestPicture from '~/components/Contests/ContestPicture'
+import Picture from '~/components/pictures/Picture'
 import { getMokshaContest } from '~/utils/getMokshaContest'
 import { getUdaanContest } from '~/utils/getUdaanContest'
 import { isNullOrUndefined } from '@arpansaha13/utils'
@@ -17,7 +17,7 @@ const RegisteredSoloContestCard = memo(({ clubSlug, contestSlug }) => {
       <Sheet className='flex flex-row-reverse lg:flex-row overflow-hidden'>
         <div className='h-36 w-36 relative'>
           {/* Replace this image with contest poster */}
-          <ContestPicture picture={contest.image} contestSlug={contest.slug} />
+          <Picture picture={contest.image} contestSlug={contest.slug} />
         </div>
 
         <div className='flex-grow px-4 sm:px-6 py-3 sm:py-4 flex flex-col'>
