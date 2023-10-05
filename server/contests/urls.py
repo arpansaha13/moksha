3a3d9eb83699431353e53a3b499f072ec5d2cfb7
arpansaha13 <arpansaha1300@gmail.com>
2023-10-06 01:39:13 +0530

    fix api url sequence

--- server/contests/urls.py
+++ server/contests/urls.py
@@ -2,7 +2,7 @@ from rest_framework.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/<slug:club_slug>/<slug:contest_slug>', GetContestRegistrations.as_view()),
     path('/solo/registration', SoloContestRegistration.as_view()),
     path('/team/registration', TeamContestRegistration.as_view()),
+    path('/<slug:club_slug>/<slug:contest_slug>', GetContestRegistrations.as_view()),
 ]
