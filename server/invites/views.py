dc6e78203aea3d237a984ecca4438e99a7df9378
arpansaha13 <arpansaha1300@gmail.com>
2023-12-15 20:05:21 +0530

    refactor: invite apis

--- server/invites/views.py
+++ server/invites/views.py
@@ -1,14 +1,14 @@
 
 from django.db.models import Q
+from django.contrib.auth.models import User
 from django.db import transaction, IntegrityError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
-from .models import Invite, InviteStatus
 from teams.models import Team, TeamMember
-from users.models import User
-from common.exceptions import Conflict, BadRequest, InternalServerError
 from teams.helpers import get_team
+from common.exceptions import Conflict, BadRequest, InternalServerError
+from .models import Invite
 from .helpers import verify_invite, verify_team_leader
 
 
@@ -18,24 +18,24 @@ class BaseEndpoint(APIView):
         team_id = request.data['team_id']
         user_id = request.data['user_id']
 
-        team = Team.objects.filter(team_id=team_id).first()
-
-        verify_team_leader(team, request.auth_user)
-
-        user = User.objects.filter(user_id=user_id).first()
-
-        if not user:
-            raise BadRequest(message='Invalid user_id')
-
-        if Invite.objects.filter(
-            Q(user_id=user_id)
-            & Q(team_id=team_id)
-            & Q(status=InviteStatus.PENDING)
-        ).exists():
-            raise Conflict(message='User has already been invited.')
-
         try:
             with transaction.atomic():
+                team = Team.objects.filter(
+                    team_id=team_id).only('leader').first()
+                verify_team_leader(team, request.user)
+
+                try:
+                    user = User.objects.get(id=user_id)
+                except User.DoesNotExist:
+                    raise BadRequest(message='Invalid user_id')
+
+                if Invite.objects.filter(
+                    Q(user_id=user_id)
+                    & Q(team_id=team_id)
+                ).exists():
+                    # User has already been invited
+                    return Response(data={'message': 'Invited'}, status=201)
+
                 if TeamMember.objects.filter(
                     Q(user=user_id)
                     & Q(team=team_id)
@@ -45,28 +45,29 @@ class BaseEndpoint(APIView):
                 new_invite = Invite(team=team, user=user)
                 new_invite.save()
         except IntegrityError:
-            raise InternalServerError(message='Some error occurred. Could not invite.')
+            raise InternalServerError(
+                message='Some error occurred. Could not invite.')
 
-        return Response({'message': 'Invited'}, status=201)
+        return Response(data={'message': 'Invited'}, status=201)
 
     # Withdraw invite
     def delete(self, request):
         team_id = request.data.get('team_id')
         user_id = request.data.get('user_id')
 
-        team = Team.objects.filter(team_id=team_id).only('leader').first()
-
-        verify_team_leader(team, request.auth_user)
-
-        if not User.objects.filter(user_id=user_id).exists():
-            raise NotFound({'message': 'User not found.'})
-
         try:
             with transaction.atomic():
+                team = Team.objects.filter(
+                    team_id=team_id).only('leader').first()
+
+                verify_team_leader(team, request.user)
+
+                if not User.objects.filter(id=user_id).exists():
+                    raise NotFound({'message': 'User not found.'})
+
                 invite = Invite.objects.filter(
-                    Q(user_id=user_id)
-                    & Q(team_id=team_id)
-                    & Q(status=InviteStatus.PENDING)
+                    Q(user=user_id)
+                    & Q(team=team_id)
                 ).first()
 
                 if not invite:
@@ -74,45 +75,45 @@ class BaseEndpoint(APIView):
 
                 invite.delete()
         except IntegrityError:
-            raise InternalServerError(message='Some error occurred. Could not withdraw invite.')
+            raise InternalServerError(
+                message='Some error occurred. Could not withdraw invite.')
 
-        return Response({'message': 'Invite has been withdrawn'}, status=200)
+        return Response(data={'message': 'Invite has been withdrawn'})
 
 
 class AcceptInvite(APIView):
     def patch(self, _, invite_id):
-        invite = Invite.objects.filter(id=invite_id).first()
+        invite = Invite.objects.get(id=invite_id)
 
         try:
             with transaction.atomic():
                 invite = verify_invite(invite)
-
-                invite.status = InviteStatus.ACCEPTED
-                invite.save()
 
                 joined_user = TeamMember(team=invite.team, user=invite.user)
                 joined_user.save()
 
                 team = get_team(invite.team.team_id)
                 team.member_count += 1
-                team.save()
-        except IntegrityError:
-            raise InternalServerError(message='Some error occurred. Could not accept invite.')
 
-        return Response({'message': 'Invite accepted'}, status=200)
+                team.save()
+                invite.delete()
+        except IntegrityError:
+            raise InternalServerError(
+                message='Some error occurred. Could not accept invite.')
+
+        return Response(data={'message': 'Invite accepted'})
 
 
 class RejectInvite(APIView):
     def patch(self, _, invite_id):
-        invite = Invite.objects.filter(id=invite_id).first()
+        invite = Invite.objects.get(id=invite_id)
 
         try:
             with transaction.atomic():
                 invite = verify_invite(invite)
-
-                invite.status = InviteStatus.REJECTED
-                invite.save()
+                invite.delete()
         except IntegrityError:
-            raise InternalServerError(message='Some error occurred. Could not reject invite.')
+            raise InternalServerError(
+                message='Some error occurred. Could not reject invite.')
 
-        return Response({'message': 'Invite rejected'}, status=200)
+        return Response(data={'message': 'Invite rejected'})
