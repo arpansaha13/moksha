0738ff18b90ef8e6f98e5829135ede0a125854c2
arpansaha13 <arpansaha1300@gmail.com>
2024-01-19 10:33:05 +0530

    docs: fix nita-got-telent description

--- client/src/data/contests/moksha/wecan.ts
+++ client/src/data/contests/moksha/wecan.ts
@@ -41,16 +41,7 @@ export const wecan: Contest[] = [
     deadline: getDateFromIST('2024-02-01', '23:59:00'),
     description: [
       {
-        p: 'Have you ever heard of a person who never watches any movies or TV shows ? Now What are you thinking about !? About the person or the movies!',
-      },
-      {
-        p: 'Besides being out in the fresh air. One theory says all the jeers, taunts, calls, rhymes and teasing that go on actually brings in joy and fun.',
-      },
-      {
-        p: 'Dumb Charades involves explaining the phrases or the name of the movie, a TV show, etc. through acting. A person is not allowed to talk and is required to act out the name by using different gestures, facial expressions, and body language.',
-      },
-      {
-        p: 'Dates and venue will be announced later.',
+        p: 'It is your stage. A set up where a person or group of people can showcase their unusual talents—dancing, singing, acting, comedy, etc.—to the judge and audience in the hopes of winning a sizable reward. Indeed, there is a golden buzzer!',
       },
     ],
   },
