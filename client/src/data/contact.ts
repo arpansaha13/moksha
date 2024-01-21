a29a4e4c427dd6ea4e909107c1ae30864735b35b
Keraskp <adityakiran.cs@gmail.com>
2024-01-21 13:30:43 +0530

    refactor: update contacts

--- client/src/data/contact.ts
+++ client/src/data/contact.ts
@@ -5,37 +5,54 @@ export interface Contact {
   email: string
 }
 
-const generalSecretary: Contact = {
-  name: 'Pratnadeep Banik',
-  designation: 'General Secretary',
-  whatsApp: '+91 70051 99236',
-  email: 'pratnadeepbanik010@gmail.com',
-}
-
-const asstGeneralSecretaries: Contact[] = [
+const contacts: Contact[] = [{
+    name: 'Pratnadeep Banik',
+    designation: 'Vice President Boys',
+    whatsApp: '+91 70051 99236',
+    email: 'pratnadeepbanik010@gmail.com',
+  },
+  {
+    name: 'Raheena Bin Mohammed',
+    designation: 'Vice President Girls',
+    whatsApp: '+91 90619 69689',
+    email: 'raheenabinmohd@gmail.com',
+  },
+  {
+    name: 'Jagreeti Chakraborty',
+    designation: 'General Secretary Cultural',
+    whatsApp: '+91 88370 38817',
+    email: 'jagreetichakraborty@gmail.com',
+  },
+  {
+    name: 'Kallol Barman',
+    designation: 'Asst. General Secretary Cultural',
+    whatsApp: '+91 87875 61427',
+    email: 'kallolbarman251@gmail.com',
+  },
+  {
+    name: 'Kolisetti Rama Rao',
+    designation: 'Asst. General Secretary Cultural',
+    whatsApp: '+91 81869 16999',
+    email: 'kolisettiramarao456@gmail.com',
+  },
   {
     name: 'Anirban Ghosh',
-    designation: 'Asst. General Secretary',
+    designation: 'Ex - Asst. General Secretary Cultural',
     whatsApp: '+91 97745 92613',
     email: 'anirbanghoshs30@gmail.com',
   },
   {
     name: 'Rumana Akter',
-    designation: 'Asst. General Secretary',
+    designation: 'Ex - Asst. General Secretary Cultural',
     whatsApp: '+91 84149 67192',
     email: 'rumanaak321@gmail.com',
   },
   {
-    name: 'Santhosh Kemsarapu',
-    designation: 'Asst. General Secretary',
+    name: 'Santhosh Kemsarapu', 
+    designation: 'Ex - Asst. General Secretary Cultural',
     whatsApp: '+91 6302 310 473',
     email: 'santhoshkemsarapu02@gmail.com',
   },
 ]
 
-const contact = Object.freeze({
-  generalSecretary,
-  asstGeneralSecretaries,
-})
-
-export default contact
+export default contacts
